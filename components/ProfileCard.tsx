48e6824b8457e709c64dad80b782fcd78d051b36
Vomesh <atukurivomesh@gmail.com>
2025-03-26 12:48:07 +0530

    Refactor ProfileCard component; adjust border styles, update text classes, and improve layout

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -16,7 +16,6 @@ export default function ProfileCard({
   pitchesLength,
   totalViews,
 }: ProfileComponentProps) {
-  
   return (
     <div className="flex flex-col items-center">
       <div className="bg-white py-3 px-6 rounded-xl text-center border-2 border-black border-b-[6px] border-r-[4px] shadow-md -mb-7 z-10 dark:bg-gray-800 dark:border-white rotate-[2deg]">
@@ -26,8 +25,8 @@ export default function ProfileCard({
       </div>
 
       <div
-        className="w-[280px] bg-gradient-to-b from-[#f03a7a] to-[#d92663] rounded-[40px] overflow-hidden border-4 
-                      border-r-[6px] border-b-[6px] border-black dark:border-white  shadow-lg"
+        className="w-[280px] bg-gradient-to-b from-[#f03a7a] to-[#d92663] rounded-[40px] overflow-hidden border-2
+                      border-r-[4px] border-b-[4px] border-black dark:border-white  shadow-lg"
       >
         <div className="pt-8 pb-6 px-6 flex flex-col items-center">
           <div className="w-[160px] h-[160px] rounded-full border-[6px] border-white mb-4 overflow-hidden shadow-lg">
@@ -42,19 +41,19 @@ export default function ProfileCard({
             </div>
           </div>
 
-          <h3 className="text-white text-xl font-bold">{`@${
+          <h3 className=" text-xl font-bold">{`@${
             email.trim().split("@")[0]
           }`}</h3>
-          <p className="text-white text-lg opacity-90 mb-4">{getRandomBio()}</p>
+          <p className="text-[12px] text-foreground opacity-90 mb-4 text-center">
+            {getRandomBio()}
+          </p>
 
           <div className="w-full h-[1px] bg-white opacity-30 my-2"></div>
 
-          <div className="flex justify-between w-full px-6 mt-2 text-white">
+          <div className="flex justify-between w-full px-6 mt-2 ">
             <div className="flex items-center gap-2">
               <Eye className="text-xl opacity-80" />
-              <span className="text-lg font-semibold">
-                {totalViews || 0}
-              </span>
+              <span className="text-lg font-semibold">{totalViews || 0}</span>
             </div>
 
             <div className="flex items-center gap-2">
@@ -67,6 +66,3 @@ export default function ProfileCard({
     </div>
   );
 }
-
-
-
